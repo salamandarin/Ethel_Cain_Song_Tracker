6c3a90099afd1848e76a1cf07b8b5fa02e64e74d
outi5 <45103903+outi5@users.noreply.github.com>
2024-04-16 19:20:16 -0500

    Added `listtracks` endpoint

---
+++ backend/database/database.go
@@ -0,0 +1,34 @@
+package ethelcaindb
+
+import (
+	"backend/models"
+	"database/sql"
+	"log"
+)
+
+func GetAllTracks(db *sql.DB) []models.Track {
+	row, err := db.Query("SELECT * FROM Track")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	var tracks []models.Track
+	for row.Next() {
+		var trackId int
+		var title string
+		var length int
+		var date string
+		var artist string
+
+		row.Scan(&trackId, &title, &length, &date, &artist)
+		track := models.Track{
+			TrackId: trackId,
+			Title:   title,
+			Length:  length,
+			Date:    date,
+			Artist:  artist,
+		}
+		tracks = append(tracks, track)
+	}
+	return tracks
+}
--- backend/main/main.go
+++ backend/main/main.go
@@ -7,6 +7,8 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
+
+	"backend/ethelcaindb"
 )
 
 var ethelCainDatabase *sql.DB
@@ -15,35 +17,37 @@ func main() {
 	router := mux.NewRouter()
 
 	var err error
-	ethelCainDatabase, err = sql.Open("sqlite3", "../database/ethelcain.db")
+	ethelCainDatabase, err = sql.Open("sqlite3", "../database/EthelCain.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ethelCainDatabase.Close()
 
 	router.HandleFunc("/", hello).Methods(http.MethodPost)
-	router.HandleFunc("/releasedate", getTrackByReleaseDateHandler).Methods(http.MethodPost)
+	router.HandleFunc("/listtracks", getTracksHandler).Methods(http.MethodPost)
+	//router.HandleFunc("/releasedate", getTrackByReleaseDateHandler).Methods(http.MethodPost)
 
 	http.ListenAndServe(":8000", router)
 }
 
-func getTrackByReleaseDateHandler(w http.ResponseWriter, r *http.Request) {
-	var schoolSupply models.SchoolSupply
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Ethel!"))
+}
 
-	err := json.NewDecoder(r.Body).Decode(&schoolSupply)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+func getTracksHandler(w http.ResponseWriter, r *http.Request) {
+	tracks := ethelcaindb.GetAllTracks(ethelCainDatabase)
 
-	newSchoolSupply := backpackdb.GetSchoolSupplyByName(lukesBackPackDatabase, schoolSupply)
-
-	newSchoolSupplyJson, err := json.Marshal(newSchoolSupply)
+	tracksJson, err := json.Marshal(tracks)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(newSchoolSupplyJson)
+	enableCors(&w)
+	w.Write(tracksJson)
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
