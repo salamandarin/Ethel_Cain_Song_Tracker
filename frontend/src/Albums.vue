fa6aeb78b9fad615ae8114a8d945313138669fed
Sam <103283741+salamandarin@users.noreply.github.com>
2024-04-24 12:37:29 -0500

    started basic album page

--- frontend/src/Albums.vue
+++ frontend/src/Albums.vue
@@ -1,7 +1,105 @@
 <script setup>
+import {ref} from 'vue'
+import AlbumCard from './components/AlbumCard.vue'
 
+// const albums = ref([])
+
+// fetch('http://localhost:8000/listalbums', {
+//     method: 'POST'
+// })
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data)
+//     data.forEach(album => {
+//         albums.value.push(album)
+//     });
+// })
+
+let AlbumId = 0;
+const albums = ref([
+    {
+        AlbumId: AlbumId++,
+        Title: 'Test Album Title',
+        Date: 'April 23, 2024',
+        Image: 'Nightmares.jpg',
+        Description: 'description',
+    },
+    {
+        AlbumId: AlbumId++,
+        Title: 'Test Album Title',
+        Date: 'April 23, 2024',
+        Image: 'Nightmares.jpg',
+        Description: 'description',
+    },
+    {
+        AlbumId: AlbumId++,
+        Title: 'Test Album Title',
+        Date: 'April 23, 2024',
+        Image: 'Nightmares.jpg',
+        Description: 'description',
+    },
+    {
+        AlbumId: AlbumId++,
+        Title: 'Test Album Title',
+        Date: 'April 23, 2024',
+        Image: 'Nightmares.jpg',
+        Description: 'description',
+    },
+    {
+        AlbumId: AlbumId++,
+        Title: 'Test Album Title',
+        Date: 'April 23, 2024',
+        Image: 'Nightmares.jpg',
+        Description: 'description',
+    },
+    {
+        AlbumId: AlbumId++,
+        Title: 'Test Album Title',
+        Date: 'April 23, 2024',
+        Image: 'Nightmares.jpg',
+        Description: 'description',
+    },
+    {
+        AlbumId: AlbumId++,
+        Title: 'Test Album Title',
+        Date: 'April 23, 2024',
+        Image: 'Nightmares.jpg',
+        Description: 'description',
+    },
+    {
+        AlbumId: AlbumId++,
+        Title: 'Test Album Title',
+        Date: 'April 23, 2024',
+        Image: 'Nightmares.jpg',
+        Description: 'description',
+    },
+    {
+        AlbumId: AlbumId++,
+        Title: 'Test Album Title',
+        Date: 'April 23, 2024',
+        Image: 'Nightmares.jpg',
+        Description: 'description',
+    },
+    {
+        AlbumId: AlbumId++,
+        Title: 'Test Album Title',
+        Date: 'April 23, 2024',
+        Image: 'Nightmares.jpg',
+        Description: 'description',
+    },
+    ])
+
+
+const tracklists = ref([])
 </script>
 
 <template>
-    <h1>Albums</h1>
-</template>
+    <v-container w-100>
+        <h1>Albums</h1>
+        <v-row>
+            <v-col cols="3" v-for="album in albums" :key="album.AlbumId">
+                <AlbumCard :title="album.Title" :date="album.Date" :image_name="album.Image">{{ album.Description }}</AlbumCard>
+            </v-col>
+        </v-row>
+    </v-container>
+</template> v
---
+++ frontend/src/components/AlbumCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    title: String,
+    date: String,
+    image_name: String
+})
+</script>
+
+<template>
+    <v-card>
+        <v-card-title>{{ title }}</v-card-title>
+        <v-card-subtitle>{{ date }}</v-card-subtitle>
+        <img width="100%" :src="`../../public/images/albums/${image_name}`">
+        <br>
+        <v-card-text><slot></slot></v-card-text>
+    </v-card>
+</template>
+
