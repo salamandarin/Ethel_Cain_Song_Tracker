6b3186dddcfe6e174698c55be640730234e9a338
Sam <103283741+salamandarin@users.noreply.github.com>
2024-05-04 22:25:15 -0500

    added song search bar + fixed 'Sunday Morning - Demo' date

--- frontend/src/Tracks.vue
+++ frontend/src/Tracks.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref} from 'vue'
+import { ref, computed } from 'vue'
 import TrackRow from './components/TrackRow.vue'
 
 const tracks = ref([])
@@ -13,13 +13,78 @@ fetch('http://localhost:8000/listtracks', {
             tracks.value.push(track)
         });
     })
+
+
+const searchInput = ref("")
+const searchResults = ref([])
+const searchError = ref("")
+
+// search for song by title
+function songSearch() {
+    // make sure old results & errors are cleared
+    searchResults.value = []
+    searchError.value = ""
+
+    fetch('http://localhost:8000/searchbysongname', {
+        method: 'POST',
+        body: JSON.stringify({
+            "TrackTitle": searchInput.value.trim()
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(track => {
+            searchResults.value.push(track)
+        });
+    })
+    .catch(error => {
+        searchError.value = `No tracks with the title '${searchInput.value.trim()}' were found`
+    });
+}
+
+// filter tracks
+const filteredTracks = computed(() => {
+    // if no search results, return all tracks
+    if (searchResults.value.length === 0) {
+        return tracks.value
+    }
+    // otherwise return search results
+    else {
+        return searchResults.value
+    }
+})
+
+// reset table
+function resetTable() {
+    searchInput.value = "" // clear search bar
+    searchResults.value = [] // clear results
+    searchError.value = ""
+}
 </script>
 
 <template>
+    <v-container class="mt-2">
+        <v-row>
+            <v-text-field
+                v-model="searchInput"
+                @keydown.enter="songSearch"
+                :error-messages="searchError"
+                label="Search"
+                prepend-inner-icon="mdi-magnify"
+                variant="outlined"
+                clearable
+                density="compact"
+                class="mr-3"
+            ></v-text-field>
+
+            <v-btn class="bg-grey-darken-3" @click="resetTable">Reset Table</v-btn>
+        </v-row>
+    </v-container>
+
     <v-table
         density="compact"
-        height="calc(100vh - 100px)" fixed-header
-        class="mx-16 my-4 rounded-lg"
+        height="calc(100vh - 160px)" fixed-header
+        class="mx-16 mb-4 rounded-lg"
     >
         <thead>
         <tr>
@@ -30,7 +95,7 @@ fetch('http://localhost:8000/listtracks', {
             </tr>
         </thead>
         <tbody>
-            <TrackRow v-for="track in tracks" :key="track.TrackId"
+            <TrackRow v-for="track in filteredTracks" :key="track.TrackId"
                 :title="track.TrackTitle"
                 :length="track.TrackLength"
                 :date="track.TrackDate"
